00be76d174c7134871667800df93839333c9e9f2
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-23 18:54:31 +0100

    add login front

--- views/login_view.php
+++ views/login_view.php
@@ -1,1 +1,23 @@
-    
+<div class="background">
+    <div class="shape"></div>
+    <div class="shape"></div>
+</div>
+<form>
+    <h3>Login Here</h3>
+
+    <label for="username">Username</label>
+    <input name="email" type="text" placeholder="Email" id="username">
+
+    <label for="password">Password</label>
+    <input name="password" type="password" placeholder="Password" id="password">
+
+    <div class="social">
+        <button name="login">Login</button>
+    </div>
+    <div class="social items-center flex-col">
+        <p class="text-sm mb-4">Or</p>
+        <a href="<?= PATH ?>index.php?page=register">
+            <div class="cursor-pointer">Sign Up</div>
+        </a>
+    </div>
+</form>
